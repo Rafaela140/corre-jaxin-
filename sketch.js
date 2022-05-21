04ea34ee747e8ff8c9af22bcabe47ef0248495a0
Rafaela140 <96181901+Rafaela140@users.noreply.github.com>
2022-05-20 22:10:35 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function preload(){
+  //imagens pré-carregadas
+}
+
+function setup(){
+  createCanvas(400,400);
+  //crie sprite aqui
+}
+
+function draw() {
+  background(0);
+
+}
+if(path.y>400){
+   path.y = Height/2;
+}
